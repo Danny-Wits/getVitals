3c83496c63109efd10c2e942dbb01cfefcaa6b86
Danishwer _ <dannywits12321@gmail.com>
2024-07-03 19:40:23 +0530

    css fix

--- src/comp/Nutrient.jsx
+++ src/comp/Nutrient.jsx
@@ -9,7 +9,7 @@ function Nutrient(props) {
    else if (currentP<=15)color='bg-yellow-500'
    else if (currentP>=100)color='bg-lime-400'
     return (
-        <span className='my-3 mx-1'>
+        <span className='my-3 mx-1 flex flex-row flex-wrap'>
        <span className="p-2 rounded-s-lg border-2 border-black bg-black">{props.nutrient}</span>
         
         <span className="p-2  font-bold border-2 border-black text-indigo-950">
