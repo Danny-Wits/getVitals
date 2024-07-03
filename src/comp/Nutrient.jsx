356ecd456377d08520ea9ca5d6e59ae0d57e8075
Danishwer _ <dannywits12321@gmail.com>
2024-07-03 19:49:41 +0530

    css fix

--- src/comp/Nutrient.jsx
+++ src/comp/Nutrient.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 function Nutrient(props) {
    let remaining = props.dsa-props.value
+   if(remaining<0)remaining=0;
    let currentP = (props.value*100)/props.dsa
    let color='bg-green-500'
    if (currentP<=1)color='bg-red-500'
    else if (currentP<=5)color='bg-orange-500'
    else if (currentP<=15)color='bg-yellow-500'
-   else if (currentP>=100)color='bg-lime-400'
+   else if (currentP>=100&&currentP<=150)color='bg-lime-400'
+   else if (currentP>150)color='bg-sky-400'
     return (
         <span className='my-3 mx-1 flex flex-row flex-wrap'>
        <span className="p-2 rounded-s-lg border-2 border-black bg-black">{props.nutrient}</span>
